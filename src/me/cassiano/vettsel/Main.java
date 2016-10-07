ec191e3cf236a0d7d9d295239a626006e2a0a7e5
Matheus C. Candido <matheus@mcassiano.com>
2016-10-07 08:43:47 -0300

    updating model

--- src/me/cassiano/vettsel/Main.java
+++ src/me/cassiano/vettsel/Main.java
@@ -16,7 +16,7 @@ public class Main {
     private static Function getTestFunction() {
         return new Function() {
 
-            double coeficients[] = {0, -7, -8.5};
+            double coeficients[] = {0, 100, 500, 45, 200, 500, 1000};
 
             @Override
             public double getCoeficient(int index) {
@@ -57,7 +57,7 @@ public class Main {
 
     private static List<Restriction> getTestRestrictions() {
 
-        double restrictions[][] = {{-16, -0.6, -0.8}, {1800, 24, 20}};
+        double restrictions[][] = {{30, 2, 2, 19, 0, 0}, {0, 0, 10, 2, 5, 10}};
         ArrayList<Restriction> restrictions1 = new ArrayList<>();
 
         for (int i = 0; i < restrictions.length; i++) {
